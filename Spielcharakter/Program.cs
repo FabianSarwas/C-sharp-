093c322e667698a35a3d81ab2c2723db6e8286a4
FabianSarwas <Fabian.Sarwas@outlook.com>
2025-11-13 09:11:31 +0100

    Second Commit

---
+++ Spielcharakter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SpielcharakterMVC
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Ein allgemeiner Charakter
+            Charakter held = new Charakter("Arthos", 100, "Leder", 3);
+            Magier zauberer = new Magier("Gandalf", 100, "Stoff", 2, 100, 3);
+            Krieger kaempfer = new Krieger("Olaf", 100, "Platte", 5, 100);
+            // Methodenaufrufe
+            held.Action();
+            zauberer.Action();
+            kaempfer.Action();
+
+
+        }
+    }
+}
